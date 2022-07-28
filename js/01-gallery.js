68567a3bbdbf7fe1b04ce2edf621c3a6a45c7987
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-07-28 14:04:51 +0300

    tast1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-
 const gallery = document.querySelector('.gallery');
-
 
 
 const galleryMarkup = galleryItems.map(el => `
