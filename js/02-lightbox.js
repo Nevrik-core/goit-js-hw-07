71ca0373c759f39e3387769f96c070604e42c224
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-07-28 14:39:03 +0300

    almost complete task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryMarkup = galleryItems.map(el => `
+
+ <a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>
+`).join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: galleryItems.description });
